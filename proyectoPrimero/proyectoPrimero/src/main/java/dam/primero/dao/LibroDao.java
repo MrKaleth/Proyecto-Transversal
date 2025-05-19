75b9e75b85e3d06e36d3e570842c64b55e25563e
MrKaleth <dariobope@gmail.com>
2025-05-19 13:39:00 +0200

    Actualización

--- proyectoPrimero/proyectoPrimero/src/main/java/dam/primero/dao/LibroDao.java
+++ proyectoPrimero/proyectoPrimero/src/main/java/dam/primero/dao/LibroDao.java
@@ -8,144 +8,159 @@ import dam.primero.modelos.Libro;
 
 public class LibroDao extends JdbcDao {
 
-    public LibroDao() throws Exception {
-        super();
-    }
+	public LibroDao() throws Exception {
+		super();
+	}
 
-    public List<Libro> obtenerLibrosBiblioteca() throws SQLException {
-        List<Libro> biblioteca = new ArrayList<>();
-        String sql = "SELECT * FROM libros";
-        try (Connection conn = getConnection();
-             PreparedStatement st = conn.prepareStatement(sql);
-             ResultSet rs = st.executeQuery()) {
+	public List<Libro> obtenerLibrosBiblioteca() throws SQLException {
+		List<Libro> biblioteca = new ArrayList<>();
+		String sql = "SELECT * FROM libros";
+		try (Connection conn = getConnection();
+				PreparedStatement st = conn.prepareStatement(sql);
+				ResultSet rs = st.executeQuery()) {
 
-            while (rs.next()) {
-                Libro l = new Libro();
-                l.setId_libro(rs.getInt("id_libro"));
-                l.setNombre_libro(rs.getString("nombre_libro"));
-                l.setAutor_libro(rs.getString("autor_libro"));
-                l.setGenero_libro(rs.getString("genero_libro"));
-                l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
-                l.setDescripcion(rs.getString("descripcion"));
-                biblioteca.add(l);
-            }
+			while (rs.next()) {
+				Libro l = new Libro();
+				l.setId_libro(rs.getInt("id_libro"));
+				l.setNombre_libro(rs.getString("nombre_libro"));
+				l.setAutor_libro(rs.getString("autor_libro"));
+				l.setGenero_libro(rs.getString("genero_libro"));
+				l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
+				l.setDescripcion(rs.getString("descripcion"));
+				biblioteca.add(l);
+			}
+		}
+		return biblioteca;
+	}
+	
+
+    public void imprimirLibros(List<Libro> libros) {
+        if (libros.isEmpty()) {
+            System.out.println("No se han encontrado libros.");
+            return;
         }
-        return biblioteca;
-    }
-
-    public boolean insertarLibro(Libro libro) throws SQLException {
-        String sql = "INSERT INTO libros(nombre_libro, autor_libro, genero_libro, anyo_publicacion, descripcion) "
-                   + "VALUES (?, ?, ?, ?, ?)";
-        try (Connection conn = getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-
-            ps.setString(1, libro.getNombre_libro());
-            ps.setString(2, libro.getAutor_libro());
-            ps.setString(3, libro.getGenero_libro());
-            ps.setInt(4, libro.getAnyo_publicacion());
-            ps.setString(5, libro.getDescripcion());
-
-            int filas = ps.executeUpdate();
-            if (filas > 0) {
-                try (ResultSet keys = ps.getGeneratedKeys()) {
-                    if (keys.next()) {
-                        libro.setId_libro(keys.getInt(1));
-                    }
-                }
-                return true;
-            }
-            return false;
+        for (Libro l : libros) {
+            System.out.println("–––––––––––––––––––––––––––––––––––");
+            System.out.printf("ID:               %d%n", l.getId_libro());
+            System.out.printf("Título:           %s%n", l.getNombre_libro());
+            System.out.printf("Autor:            %s%n", l.getAutor_libro());
+            System.out.printf("Género:           %s%n", l.getGenero_libro());
+            System.out.printf("Año publicación:  %d%n", l.getAnyo_publicacion());
+            System.out.printf("Descripción:      %s%n", l.getDescripcion());
         }
+        System.out.println("–––––––––––––––––––––––––––––––––––\n");
     }
 
-    public List<Libro> buscarPorTitulo(String titulo) throws SQLException {
-        List<Libro> lista = new ArrayList<>();
-        String sql = "SELECT * FROM libros WHERE nombre_libro LIKE ?";
-        try (Connection conn = getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
 
-            ps.setString(1, "%" + titulo + "%");
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    Libro l = new Libro();
-                    l.setId_libro(rs.getInt("id_libro"));
-                    l.setNombre_libro(rs.getString("nombre_libro"));
-                    l.setAutor_libro(rs.getString("autor_libro"));
-                    l.setGenero_libro(rs.getString("genero_libro"));
-                    l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
-                    l.setDescripcion(rs.getString("descripcion"));
-                    lista.add(l);
-                }
-            }
-        }
-        return lista;
-    }
+	public boolean insertarLibro(Libro libro) throws SQLException {
+		String sql = "INSERT INTO libros(nombre_libro, autor_libro, genero_libro, anyo_publicacion, descripcion) "
+				+ "VALUES (?, ?, ?, ?, ?)";
+		try (Connection conn = getConnection();
+				PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
-    public List<Libro> buscarPorAutor(String autor) throws SQLException {
-        List<Libro> lista = new ArrayList<>();
-        String sql = "SELECT * FROM libros WHERE autor_libro LIKE ?";
-        try (Connection conn = getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+			ps.setString(1, libro.getNombre_libro());
+			ps.setString(2, libro.getAutor_libro());
+			ps.setString(3, libro.getGenero_libro());
+			ps.setInt(4, libro.getAnyo_publicacion());
+			ps.setString(5, libro.getDescripcion());
 
-            ps.setString(1, "%" + autor + "%");
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    Libro l = new Libro();
-                    l.setId_libro(rs.getInt("id_libro"));
-                    l.setNombre_libro(rs.getString("nombre_libro"));
-                    l.setAutor_libro(rs.getString("autor_libro"));
-                    l.setGenero_libro(rs.getString("genero_libro"));
-                    l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
-                    l.setDescripcion(rs.getString("descripcion"));
-                    lista.add(l);
-                }
-            }
-        }
-        return lista;
-    }
+			int filas = ps.executeUpdate();
+			if (filas > 0) {
+				try (ResultSet keys = ps.getGeneratedKeys()) {
+					if (keys.next()) {
+						libro.setId_libro(keys.getInt(1));
+					}
+				}
+				return true;
+			}
+			return false;
+		}
+	}
 
-    public List<Libro> buscarPorGenero(String genero) throws SQLException {
-        List<Libro> lista = new ArrayList<>();
-        String sql = "SELECT * FROM libros WHERE genero_libro LIKE ?";
-        try (Connection conn = getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+	public List<Libro> buscarPorTitulo(String titulo) throws SQLException {
+		List<Libro> lista = new ArrayList<>();
+		String sql = "SELECT * FROM libros WHERE nombre_libro LIKE ?";
+		try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
 
-            ps.setString(1, "%" + genero + "%");
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    Libro l = new Libro();
-                    l.setId_libro(rs.getInt("id_libro"));
-                    l.setNombre_libro(rs.getString("nombre_libro"));
-                    l.setAutor_libro(rs.getString("autor_libro"));
-                    l.setGenero_libro(rs.getString("genero_libro"));
-                    l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
-                    l.setDescripcion(rs.getString("descripcion"));
-                    lista.add(l);
-                }
-            }
-        }
-        return lista;
-    }
+			ps.setString(1, "%" + titulo + "%");
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					Libro l = new Libro();
+					l.setId_libro(rs.getInt("id_libro"));
+					l.setNombre_libro(rs.getString("nombre_libro"));
+					l.setAutor_libro(rs.getString("autor_libro"));
+					l.setGenero_libro(rs.getString("genero_libro"));
+					l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
+					l.setDescripcion(rs.getString("descripcion"));
+					lista.add(l);
+				}
+			}
+		}
+		return lista;
+	}
 
-    public Libro obtenerPorId(int id) throws SQLException {
-        String sql = "SELECT * FROM libros WHERE id_libro = ?";
-        try (Connection conn = getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+	public List<Libro> buscarPorAutor(String autor) throws SQLException {
+		List<Libro> lista = new ArrayList<>();
+		String sql = "SELECT * FROM libros WHERE autor_libro LIKE ?";
+		try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
 
-            ps.setInt(1, id);
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    Libro l = new Libro();
-                    l.setId_libro(rs.getInt("id_libro"));
-                    l.setNombre_libro(rs.getString("nombre_libro"));
-                    l.setAutor_libro(rs.getString("autor_libro"));
-                    l.setGenero_libro(rs.getString("genero_libro"));
-                    l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
-                    l.setDescripcion(rs.getString("descripcion"));
-                    return l;
-                }
-            }
-        }
-        return null;
-    }
+			ps.setString(1, "%" + autor + "%");
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					Libro l = new Libro();
+					l.setId_libro(rs.getInt("id_libro"));
+					l.setNombre_libro(rs.getString("nombre_libro"));
+					l.setAutor_libro(rs.getString("autor_libro"));
+					l.setGenero_libro(rs.getString("genero_libro"));
+					l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
+					l.setDescripcion(rs.getString("descripcion"));
+					lista.add(l);
+				}
+			}
+		}
+		return lista;
+	}
+
+	public List<Libro> buscarPorGenero(String genero) throws SQLException {
+		List<Libro> lista = new ArrayList<>();
+		String sql = "SELECT * FROM libros WHERE genero_libro LIKE ?";
+		try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			ps.setString(1, "%" + genero + "%");
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					Libro l = new Libro();
+					l.setId_libro(rs.getInt("id_libro"));
+					l.setNombre_libro(rs.getString("nombre_libro"));
+					l.setAutor_libro(rs.getString("autor_libro"));
+					l.setGenero_libro(rs.getString("genero_libro"));
+					l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
+					l.setDescripcion(rs.getString("descripcion"));
+					lista.add(l);
+				}
+			}
+		}
+		return lista;
+	}
+
+	public Libro obtenerPorId(int id) throws SQLException {
+		String sql = "SELECT * FROM libros WHERE id_libro = ?";
+		try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			ps.setInt(1, id);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					Libro l = new Libro();
+					l.setId_libro(rs.getInt("id_libro"));
+					l.setNombre_libro(rs.getString("nombre_libro"));
+					l.setAutor_libro(rs.getString("autor_libro"));
+					l.setGenero_libro(rs.getString("genero_libro"));
+					l.setAnyo_publicacion(rs.getInt("anyo_publicacion"));
+					l.setDescripcion(rs.getString("descripcion"));
+					return l;
+				}
+			}
+		}
+		return null;
+	}
 }
--- proyectoPrimero/proyectoPrimero/src/main/java/dam/primero/dao/PruebasDao.java
+++ proyectoPrimero/proyectoPrimero/src/main/java/dam/primero/dao/PruebasDao.java
@@ -1,13 +1,25 @@
 package dam.primero.dao;
 
+import java.util.List;
+
+import dam.primero.modelos.Libro;
+
 public class PruebasDao {
 	public static void main(String[] args) {
 		try {
 			LibroDao libroDao = new LibroDao();
 			
-			System.out.println(libroDao.buscarPorAutor("Brandon Sanderson"));
-			System.out.println(libroDao.buscarPorGenero("Fantasía"));
-			System.out.println(libroDao.buscarPorTitulo("Palabras Radiantes"));
+			List<Libro> l1 = libroDao.buscarPorAutor("Brandon Sanderson");
+			List<Libro> l2 = libroDao.buscarPorGenero("Fantasía");
+			List<Libro> l3 = libroDao.buscarPorTitulo("Palabras Radiantes");
+			
+			System.out.println("Libros por Brandon Sanderson");
+			libroDao.imprimirLibros(l1);
+			System.out.println();
+			System.out.println("Libros de Fantasía");
+			libroDao.imprimirLibros(l2);
+			System.out.println("Busqueda de Libro concreto");
+			libroDao.imprimirLibros(l3);
 
 		} catch (Exception e) {
 			e.printStackTrace();
